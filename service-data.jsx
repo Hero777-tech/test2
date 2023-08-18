5084dff0d82044f12c7b9383a3f5905ac65baccd
Hero777-tech <Hero777-tech@users.noreply.github.com>
2023-08-18 04:40:26 +0000

    deploy: aff2376ec0444368d6db128ae093938dde2a7712

---
+++ service-data.jsx
@@ -0,0 +1,47 @@
+// service-data.jsx
+const servicesData = [
+  //array of data added here for card representation in the website service section
+  {
+    id: 1,
+    name: "Web Design",
+    // image: "https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    image: "https://images.unsplash.com/photo-1552581234-26160f608093?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
+    description: "We create stunning and responsive websites tailored to your brand.",
+    
+  },
+  {
+    id: 2,
+    name: "Social Media Marketing",
+    description: "Our experts optimize your social media presence for maximum engagement.",
+    // image: "social-media-icon.png"
+    image: "https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1771&q=80"
+  },
+  {
+    id: 3,
+    name: "SEO Optimization",
+    description: "Improve your website's search engine ranking with our SEO strategies.",
+    // image: "seo-icon.png"
+    image: "https://images.unsplash.com/photo-1686061593213-98dad7c599b9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2832&q=80"
+  },
+  {
+    id: 4,
+    name: "Chatbot AI Marketing",
+    description: "Engage your audience and automate interactions with AI-powered chatbots.",
+    image: "https://st4.depositphotos.com/10325516/41085/i/600/depositphotos_410858532-stock-photo-automation-customer-service-concept-rendering.jpg" 
+  },
+  {
+    id: 5,
+    name: "Ad Design",
+    description: "Create captivating and effective ads to promote your brand.",
+    image: "https://st3.depositphotos.com/13349494/16951/i/600/depositphotos_169517612-stock-photo-autumn-sale-concept.jpg" 
+  },
+  {
+    id: 6,
+    name: "Video Marketing",
+    description: "Tell your story through compelling videos that resonate with your audience.",
+    image: "https://st3.depositphotos.com/13194036/33236/i/600/depositphotos_332363764-stock-photo-bearded-art-editor-pointing-finger.jpg" 
+  }
+  
+];
+
+export default servicesData;
